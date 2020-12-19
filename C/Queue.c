fea7b03c1dbcd535546fc1eb9934e0f27d6e1194
met <64790786+memetrusidovski@users.noreply.github.com>
2020-12-18 23:24:42 -0500

    added to queue

--- C/Queue.c
+++ C/Queue.c
@@ -8,27 +8,49 @@ typedef struct Data{
 } Data;
 
 //Sorted List Node
-typedef struct node{
+typedef struct Node{
     //The lower the priority the more important the node
-    int priority;
     Data* data;
-    struct node *ptr;
-} node_queue;
+    struct Node *ptr;
+} Node;
 
-//----- LINKED LIST -----
+//----- LINKED QUEUE -----
 typedef struct Queue {
     int height;
-    Queue* head;
-    Queue* tail;
+    Node* _front;
+    Node* _back;
 } Queue;
 
 Queue* create(){
     Queue* queue = (Queue*) malloc(sizeof(Queue));
     queue->height = 0;
-    
+
     return queue;
 }
 
+void push(Queue* queue, int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if(queue->height == 0){
+        queue->_front = newNode;
+        queue->_back = newNode;
+    }
+    else{
+        queue->_back->ptr = newNode;
+    }
+
+    queue->height++;
+}
+
+
+Node* dequeue(Queue* queue){
+    Node* temp = (Node*)malloc(sizeof(Node));
+
+    temp = queue->_front;
+    queue->_front = queue->_front->ptr;
+
+    return temp;
+}
 int main(void){
 
 }
